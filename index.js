1e6c58eca9ac1d30c76cdee8d90eac7109c0b2d7
Farhan Hasan <farhan.design@outlook.com>
2020-05-30 14:51:17 +0500

    Section 1, created Web Server, Added body-parser

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
-console.log('Hi there');
+const express = require('express');
+const bodyParser = require('body-parser');
+
+const app = express();
+
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.get('/', (req, res) => {
+  res.send(`
+    <div>
+      <form method="POST">
+        <input name="email" placeholder="email" />
+        <input name="password" placeholder="password" />
+        <input name="passwordConfirmation" placeholder="password confirmation" />
+        <button>Signup</button>
+      </form>
+    </div>
+  `);
+});
+
+
+
+app.post('/', (req, res) => {
+  console.log(req.body);
+  res.send('Account created!!');
+});
+
+app.listen(4000, () => {
+  console.log('Listening');
+});
