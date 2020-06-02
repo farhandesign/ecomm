4999387e2693bc6c151615505bf7fd1cb85e60bb
Farhan Hasan <farhan.design@outlook.com>
2020-06-02 14:41:38 +0500

    Refactor, layout, express-validator

---
+++ views/admin/layout.js
@@ -0,0 +1,14 @@
+module.exports = ({ content }) => {
+  return `
+    <!DOCTYPE html>
+    <html lang="en">
+      <head>
+        <meta charset="UTF-8" />
+        <title>Document</title>
+      </head>
+      <body>
+        ${content}
+      </body>
+    </html>
+  `;
+};
