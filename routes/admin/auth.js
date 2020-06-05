906c1116327715d83399821926fe66f4ed08f723
Farhan Hasan <farhan.design@outlook.com>
2020-06-05 15:35:58 +0500

    Redirect on success actions / requireAuth middleware

--- routes/admin/auth.js
+++ routes/admin/auth.js
@@ -31,7 +31,7 @@ router.post('/signup',
 
     req.session.userId = user.id; // .session Added by cookie-session
 
-    res.send('Account created!!');
+    res.redirect('/admin/products');
   }
 );
 
@@ -55,7 +55,7 @@ router.post('/signin',
 
     req.session.userId = user.id;
 
-    res.send('You are signed in!!!');
+    res.redirect('/admin/products');
   }
 );
 
